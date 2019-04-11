33470b41a2349144bb65375d14badc9fb5e742f0
johnlinder92 <john.linder92@gmail.com>
2019-04-11 11:27:38 +0200

    la till testdirectory och testar git funkar

---
+++ Test/IHDTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IHDTest {
+
+
+
+
+
+}

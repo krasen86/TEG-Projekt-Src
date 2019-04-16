1fffc8a9ff9d88b5fe45af425d9fd152047bd516
Krasen Parvanov <krasenparvanov@gmail.com>
2019-04-16 13:20:34 +0200

    Version 1.1-Update from the development team

--- src/IHD.java
+++ src/IHD.java
@@ -1,45 +1,60 @@
 //IHD
 
 public class IHD {
-    private int read1 = 0;      //input from sensor, "original output"
-    private int read2 = 0;      //input from sensor, "safety output"
-    private int mean  = 0;      //mean of read1 and read2
-    public int outputToSensor;  //output to sensor
-    private Sensor sensor;      //
+    private int read1;                //input from sensor, "original output"
+    private int read2;                //input from sensor, "safety output"
+    private int mean;                 //mean of read1 and read2
+    public static int outputToSensor; //output to sensor
+    private static Sensor sensor;     //the connected sensor
 
-    private const int TO_SENSOR_3V = 3;
-    private const int TO_SENSOR_0V = 0;
+    final private int TO_SENSOR_3V = 3; //3V = "reset sensor"
+    final private int TO_SENSOR_0V = 0; //0V = "listen to sensor-values"
 
-    public void initialzie(){
-
+    //Initialize IHD, connect with 1 Sensor
+    public void initialize(Sensor sensor_1){
+        read1 = 0;
+        read2 = 0;
+        mean = 0;
+        outputToSensor = TO_SENSOR_3V;
+        sensor = sensor_1;
     }
 
+    //Execute IHD
     public void execute(){
-
+        this.listenToSensor();
+        this.readFromSensor();
     }
 
+    //Calculate mean
     private int mean(int i1, int i2){
         mean = (i1+i2)/2;
-    }
-
-    public int getOutput(){
         return mean;
     }
 
     //Put 3V on "start/restart"-pin of sensor
-    public void startSensor(){
+    private void startSensor(){
         outputToSensor = TO_SENSOR_3V;
     }
 
     //Put 0V on "start/restart"-pin of sensor
-    public void readSensor(){
+    private void listenToSensor(){
         outputToSensor = TO_SENSOR_0V;
     }
 
-    public static void getInputFromSensor(){
-        this.readSensor();
+    //Collect data from connected sensor
+    private void readFromSensor(){
         read1 = sensor.originalOutput;
         read2 = sensor.safetyOutput;
     }
 
+    //Get-methods for testers
+    public int getRead1(){
+        return read1;
+    }
+    public int getRead2(){
+        return read2;
+    }
+    public int getMean(){
+        return mean;
+    }
 }

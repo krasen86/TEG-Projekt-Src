ad1c666ab6292fd22f34c3930162707ac68750a4
minwuh0811 <your_email@youremail.com>
2019-04-25 00:31:44 +0200

    Updated-mockito

--- Test/IADTest.java
+++ Test/IADTest.java
@@ -1,22 +1,115 @@
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class IADTest {
+    IHD mock_ihd1 = mock(IHD.class);
+    IHD mock_ihd2 = mock(IHD.class);
+    IAD iad = new IAD();
+
 
     @Test
-    void initialize () {
+    void AnalysesIAD2BothForwardLeftAndLeft29() throws Exception {
+        doReturn(29).when(mock_ihd1).getOutput();
+        doReturn(29).when(mock_ihd2).getOutput();
+        boolean[] expect = {false, false};
+        iad.initialize(mock_ihd1, mock_ihd2);
+        iad.execute();
+        assertArrayEquals(expect, iad.getData());
+    }
+    @Test
+    void AnalysesIAD2ForwardLeft31andLeft29 () {
+        doReturn(31).when(mock_ihd1).getOutput();
+        doReturn(29).when(mock_ihd2).getOutput();
+        boolean[] expect = {true, false};
+        iad.initialize(mock_ihd1, mock_ihd2);
+        iad.execute();
+        assertArrayEquals(expect, iad.getData());
     }
 
     @Test
-    void execute () {
+    void AnalysesIAD2ForwardLeft29andLeft31 () {
+        doReturn(29).when(mock_ihd1).getOutput();
+        doReturn(31).when(mock_ihd2).getOutput();
+        boolean[] expect = {false, true};
+        iad.initialize(mock_ihd1, mock_ihd2);
+        iad.execute();
+        assertArrayEquals(expect, iad.getData());
     }
 
     @Test
-    void analyzeData () {
+    void AnalysesIAD2BothForwardLeftAndLeft31 () {
+        doReturn(31).when(mock_ihd1).getOutput();
+        doReturn(31).when(mock_ihd2).getOutput();
+        boolean[] expect = {true, true};
+        iad.initialize(mock_ihd1, mock_ihd2);
+        iad.execute();
+        assertArrayEquals(expect, iad.getData());
     }
 
     @Test
-    void sendData () {
+    void AnalysesIAD3BothForwardRightAndRight29 () {
+        doReturn(29).when(mock_ihd1).getOutput();
+        doReturn(29).when(mock_ihd2).getOutput();
+        boolean[] expect = {false, false};
+        iad.initialize(mock_ihd1, mock_ihd2);
+        iad.execute();
+        assertArrayEquals(expect, iad.getData());
     }
+
+    @Test
+    void AnalysesIAD3ForwardRight31AndRight29 () {
+        doReturn(31).when(mock_ihd1).getOutput();
+        doReturn(29).when(mock_ihd2).getOutput();
+        boolean[] expect = {true, false};
+        iad.initialize(mock_ihd1, mock_ihd2);
+        iad.execute();
+        assertArrayEquals(expect, iad.getData());
+    }
+
+    @Test
+    void AnalysesIAD3ForwardRight29AndRight31 () {
+        doReturn(29).when(mock_ihd1).getOutput();
+        doReturn(31).when(mock_ihd2).getOutput();
+        boolean[] expect = {false, true};
+        iad.initialize(mock_ihd1, mock_ihd2);
+        iad.execute();
+        assertArrayEquals(expect, iad.getData());
+    }
+    @Test
+    void AnalysesIAD3BothForwardRightAndRight31 () {
+        doReturn(31).when(mock_ihd1).getOutput();
+        doReturn(31).when(mock_ihd2).getOutput();
+        boolean[] expect = {true, true};
+        iad.initialize(mock_ihd1, mock_ihd2);
+        iad.execute();
+        assertArrayEquals(expect, iad.getData());
+    }
+    @Test
+    void AnalysesIAD1Input4 () {
+        doReturn(4).when(mock_ihd1).getOutput();
+        boolean[] expect = {false, false};
+        iad.initialize(mock_ihd1);
+        iad.execute();
+        assertArrayEquals(expect, iad.getData());
+    }
+    @Test
+    void AnalysesIAD1Input5 () {
+        doReturn(5).when(mock_ihd1).getOutput();
+        boolean[] expect = {false, false};
+        iad.initialize(mock_ihd1);
+        iad.execute();
+        assertArrayEquals(expect, iad.getData());
+    }
+    @Test
+    void AnalysesIAD1Input6 () {
+        doReturn(6).when(mock_ihd1).getOutput();
+        boolean[] expect = {true, true};
+        iad.initialize(mock_ihd1);
+        iad.execute();
+        assertArrayEquals(expect, iad.getData());
+    }
+//
 }

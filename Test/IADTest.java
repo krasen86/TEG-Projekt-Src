11e4efeaada5cdc2d0b4f3db55675246a5b0b7f8
johnlinder92 <john.linder92@gmail.com>
2019-04-25 12:54:03 +0200

    Updated IADtest with some comments to make tests easyer to understand and allso added one last unit-test that has different values on different IHDs. Like in real life sensors values will differ, and in last test we test that the analyze handles that.

--- Test/IADTest.java
+++ Test/IADTest.java
@@ -14,7 +14,8 @@ class IADTest {
     IHD mock_ihd1 = mock(IHD.class);
     IHD mock_ihd2 = mock(IHD.class);
 
-
+// These two first test compares two iad objects, the first object is initialized with initialize method, the second
+    // is initialized locally in the test with the concstructor, and we check that their values are the same.
     @Test
     void initialize_testingInitializeMethodTwoSensor_expectFalse() {
         iad.initialize(ihd1, ihd2);
@@ -69,7 +70,7 @@ class IADTest {
         System.out.println("");
         System.out.println("Printing out result of boundary values test with two IHD");
 
-        // after initialize inputs are set to zero, here inputs is changed with array content at index t
+
         for (int t = 0; t < testData.length; t++) {
 
             // here i mock the getoutput values with the array input
@@ -77,7 +78,7 @@ class IADTest {
             doReturn(testData[t]).when(mock_ihd1).getOutput();
             doReturn(testData[t]).when(mock_ihd2).getOutput();
 
-            // then i run the execute which analyze the data
+            // then i run the execute which analyze the data with the values from array
             iad.execute();
 
 
@@ -131,13 +132,13 @@ class IADTest {
 
         System.out.println("");
         System.out.println("Printing out result of boundary values test with one IHD");
-        // after initialize inputs are set to zero, here inputs is changed with array content at index t
+
         for (int t = 0; t < testData.length; t++) {
 
             // here i mock the getoutput values with the array input
 
             doReturn(testData[t]).when(mock_ihd1).getOutput();
-         //   doReturn(testData[t]).when(mock_ihd2).getOutput();
+
 
             // then i run the execute which analyze the data
             iad.execute();
@@ -157,7 +158,64 @@ class IADTest {
 
         }
     }
+
+    @Test
+    void execute_testingLotsOfBoundaryValuesWIthTwoIHDWithdifferentIHDValues() {
+
+// here i initialize the mocked ihds
+
+        iad.initialize(mock_ihd1, mock_ihd2);
+
+
+// here i create array with data that i send in to test
+        int testData[] = {29, 31, 29, 31 };
+        int testData2[] = {29, 29, 31, 31 };
+
+        // in this test the output is two booleans, the output might change for each loop, so i must create an array of arrays that i
+        // store the expected results in. so here for example when value zero which is the first value in the testData array is sent
+        // to the for loop, once it passed the system and analyze has given it two booleans,
+        // it will be compared to the test0 array in the array of arrays called expectedResults.
+        boolean test0[] = {false, false};
+        boolean test1[] = {true, false};
+        boolean test2[] = {false, true};
+        boolean test3[] = {true, true};
+
+
+
+        boolean[] expectedResult[] = {test0, test1, test2, test3};
+
+
+        System.out.println("");
+        System.out.println("Printing out result of boundary values test with two IHD- different values in each IHD");
+
+        // after initialize inputs are set to zero, here inputs is changed with array content at index t
+        for (int t = 0; t < testData.length; t++) {
+
+            // here i mock the getoutput values with the array input
+
+            doReturn(testData[t]).when(mock_ihd1).getOutput();
+            doReturn(testData2[t]).when(mock_ihd2).getOutput();
+
+            // then i run the execute which analyze the data
+            iad.execute();
+
+
+            // then i take the result and store it in an array.
+            boolean[] booleansProducedByAnalyzeMethod = iad.getData();
+// here i store the expectedResultvalue on position t in the testet array
+            boolean testet[] = expectedResult[t];
+// and last but not the least i compare both arrays and print out result.
+
+            if (Arrays.equals(testet, booleansProducedByAnalyzeMethod))
+                System.out.println("Pass boundarytesttwoIHDDiffValue" + " " + t);
+            else
+                System.out.println("Fail boundarytesttwoIHDDiffValue" + " " + t);
+
+
+        }
+    }
 //Koden saknar nullhantering för denna modul därför är detta test inte genomförbart.
+    // code is missing null-handling for this module and therefore this test isnt applicable.
 /*    @Test
     void execute_testingAnalyzeWithNull() {
 
